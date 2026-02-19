983ca9ed384adc3041954ba5f3d7c9d35f6a11e6
skycode9 <skycode9005@gmail.com>
2026-02-19 14:22:23 -0700

    add config

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { PORT, NODE_ENV } = process.env;
+
+export const Config = {
+    PORT,
+    NODE_ENV,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,3 @@
-function welcome(name: string) {
-    console.log('Welcome', name);
-    console.log('Check lint');
-    const user = {
-        name: 'sky',
-    };
-    //user['name']
-    const u1 = user.name;
-    return name + u1;
-}
+import { Config } from './config/index.js';
 
-welcome('Akash');
+console.log(Config.PORT);
